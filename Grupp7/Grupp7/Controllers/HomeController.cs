0765f95e5257290a7d5d802851da515c85088a01
lite1701 <60007815+lite1701@users.noreply.github.com>
2020-01-28 12:41:36 +0100

    Revert "Branchlinda27jan"

--- Grupp7/Grupp7/Controllers/HomeController.cs
+++ Grupp7/Grupp7/Controllers/HomeController.cs
@@ -29,57 +29,6 @@ namespace Grupp7.Controllers
             return View();
         }
 
-        public IActionResult Register ()
-        {
-            // Skicka en en person modell här, alternativt en vymodel.
-
-            return View();
-        }
-        /*
-        public async Task<ActionResult> RegisterFreelancer(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var freelancer = new Freelancer();
-                Person = model.Person;
-                freelancer.aspnetusers_id = user.Id;
-                
-                Ändra kod ovan till att vara en användare, om identity inte används ta bort.
-                
-
-                var dbContext = new dbContext();
-
-                Db context hamnar här
-
-                var result = await UserManager.CreateAsync(user, model.Password);
-
-                
-                if (result.Succeeded) - om model state är valid
-                {
-                    UserManager.AddToRole(user.Id, "Freelancer");
-                    dbContext.Person.Add(Person);
-                    dbContext.SaveChanges();
-
-                    Lägg till person till databas.
-
-                    UserManager.AddClaim(user.Id, new Claim(ClaimTypes.GivenName, model.Person.Firstname));
-
-                    Cachea användarens namn
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-                    return RedirectToAction("Index", "Home");
-                    Retunera ny vy.
-                }
-                AddErrors(result);
-            }
-            return View(model);
-        }
-
-
-            Mycket möjligt att allt detta ska göras utanför controlern. 
-    */
-
         public IActionResult Privacy()
         {
             return View();
