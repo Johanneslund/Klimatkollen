82cbb97a2d457555275f6e782a364c6d0ec7806d
Johannes Lundkvist <johanneslundkvist@hotmail.com>
2020-01-31 12:51:18 +0100

    Liten förändring

--- Grupp7/Grupp7/Interfaces/IRepository.cs
+++ Grupp7/Grupp7/Interfaces/IRepository.cs
@@ -15,5 +15,7 @@ namespace Grupp7.Interfaces
         List<Animal> GetAnimals();
         Animal getAnimal(int id);
         void updateAimal(Animal animal);
+
+        List<Weather> GetWeathers();
     }
 }
