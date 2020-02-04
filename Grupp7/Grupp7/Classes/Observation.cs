0b70bbd14dab4baf2c99095ad370ee2261a677ef
sabrinathao <sath1801@student.miun.se>
2020-02-04 19:34:39 +0700

    Ändrat observation .

--- Grupp7/Grupp7/Classes/Observation.cs
+++ Grupp7/Grupp7/Classes/Observation.cs
@@ -8,17 +8,8 @@ namespace Grupp7.Classes
 {
     public class Observation
     {
-        public DateTime Datetime { get; set; }
-        public string Longitude { get; set; }
-        public string Latitude { get; set; }
+        public DateTime Datetime { get; set; }      
         public Animal Animal { get; set; }
         public Weather Weather { get; set; }
-
-        [ForeignKey ("AnimalId")]
-        public int AnimalId { get; set; }
-
-        [ForeignKey("WeatherId")]
-        public int WeatherId { get; set; }
-
     }
 }
