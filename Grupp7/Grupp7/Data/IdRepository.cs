b221a5da41e4d02a26b8cdfc2da24516df3cce3f
Johannes Lundkvist <johanneslundkvist@hotmail.com>
2020-02-13 11:32:15 +0100

    Ses

---
+++ Grupp7/Grupp7/Data/IdRepository.cs
@@ -0,0 +1,20 @@
+﻿using Grupp7.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grupp7.Data
+{
+    public class IdRepository : IIdRepository
+    {
+        private IdDbContext context;
+
+        public IdRepository(IdDbContext context)
+        {
+            this.context = context;
+        }
+
+        
+    }
+}
---
+++ Grupp7/Grupp7/Interfaces/IIdRepository.cs
@@ -0,0 +1,15 @@
+﻿using Grupp7.Classes;
+using Grupp7.Models;
+using Grupp7.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grupp7.Interfaces
+{
+    public interface IIdRepository
+    {
+    }
+}
